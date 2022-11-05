36fa7e96b90a6f1e628fb2d5984dfbf84740fde8
ALEX GITARI <agitari65@gmail.com>
2022-11-06 00:51:21 +0300

    debugging and added firebase database queries on th requests.

--- DriverSelectMechanic.java
+++ DriverSelectMechanic.java
@@ -27,14 +27,18 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -86,6 +90,14 @@ public class DriverSelectMechanic extends AppCompatActivity {
     String current_userId;
     String provider;
     protected String latitude, longitude;
+    private Context mContext;
+    private Activity mActivity;
+
+    private LinearLayout mRelativeLayout;
+    private Button mButton;
+
+    private PopupWindow mPopupWindow;
+
 
     FirebaseFirestore mAuth;
     NotificationManager mNotificationManager;
@@ -177,6 +189,7 @@ public class DriverSelectMechanic extends AppCompatActivity {
         buttonrequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                RequestPopUpWindow();
                 //FIREBASE REALTIME
                 DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy @ HH:mm", Locale.US);
                 String date = dateFormat.format(new Date());
@@ -399,6 +412,82 @@ buttonrequest.setVisibility(View.VISIBLE);
 
                     }
                 });
+    }
+    public void RequestPopUpWindow(){
+
+        // Get the application context
+        mContext = getApplicationContext();
+
+        // Get the activity
+        mActivity = DriverSelectMechanic.this;
+
+        // Get the widgets reference from XML layout
+        mRelativeLayout =  findViewById(R.id.rl);
+        mButton = (Button) findViewById(R.id.btnsendRequest);
+
+        // Set a click listener for the text view
+
+                // Initialize a new instance of LayoutInflater service
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+                // Inflate the custom layout/view
+                View customView = inflater.inflate(R.layout.custom_layout,null);
+
+                /*
+                    public PopupWindow (View contentView, int width, int height)
+                        Create a new non focusable popup window which can display the contentView.
+                        The dimension of the window must be passed to this constructor.
+
+                        The popup does not provide any background. This should be handled by
+                        the content view.
+
+                    Parameters
+                        contentView : the popup's content
+                        width : the popup's width
+                        height : the popup's height
+                */
+                // Initialize a new instance of popup window
+                mPopupWindow = new PopupWindow(
+                        customView,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+
+                // Set an elevation value for popup window
+                // Call requires API level 21
+                if(Build.VERSION.SDK_INT>=21){
+                    mPopupWindow.setElevation(5.0f);
+                }
+
+                // Get a reference for the custom view close button
+                ImageButton closeButton = (ImageButton) customView.findViewById(R.id.ib_close);
+
+                // Set a click listener for the popup window close button
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Dismiss the popup window
+                        mPopupWindow.dismiss();
+                    }
+                });
+
+                /*
+                    public void showAtLocation (View parent, int gravity, int x, int y)
+                        Display the content view in a popup window at the specified location. If the
+                        popup window cannot fit on screen, it will be clipped.
+                        Learn WindowManager.LayoutParams for more information on how gravity and the x
+                        and y parameters are related. Specifying a gravity of NO_GRAVITY is similar
+                        to specifying Gravity.LEFT | Gravity.TOP.
+
+                    Parameters
+                        parent : a parent view to get the getWindowToken() token from
+                        gravity : the gravity which controls the placement of the popup window
+                        x : the popup's x location offset
+                        y : the popup's y location offset
+                */
+                // Finally, show the popup window at the center location of root relative layout
+                mPopupWindow.showAtLocation(mRelativeLayout, Gravity.CENTER,0,0);
+
     }
     public  void mainLocationUpdate(){
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(DriverSelectMechanic.this);
